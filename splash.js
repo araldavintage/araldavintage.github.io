184cc9f6ce7724743ec783cc318dae6ad6609f05
Andrew D'Amico <damicoat@gmail.com>
2016-12-13 22:11:34 -0800

    Added new fonts.

--- splash.js
+++ splash.js
@@ -1,16 +1,16 @@
-(function () {
-
-  var splash = {
-    changeBackground: function () {
-      var background = document.getElementById('carousel');
-      background.style.backgroundImage = "url(\'./assets/" + splash.photoNum + ".jpeg\')";
-      splash.updatePhotoNum();
-    },
-
-    updatePhotoNum: function () {
-      splash.photoNum = Math.floor(Math.random() * 5) + 1;
-    }
-  };
-  splash.photoNum = 2;
-  setInterval(splash.changeBackground, 9000);
-})();
+// (function () {
+//
+//   var splash = {
+//     changeBackground: function () {
+//       var background = document.getElementById('carousel');
+//       background.style.backgroundImage = "url(\'./assets/" + splash.photoNum + ".jpeg\')";
+//       splash.updatePhotoNum();
+//     },
+//
+//     updatePhotoNum: function () {
+//       splash.photoNum = Math.floor(Math.random() * 5) + 1;
+//     }
+//   };
+//   splash.photoNum = 2;
+//   setInterval(splash.changeBackground, 9000);
+// })();
